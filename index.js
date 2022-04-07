c4dc0e3889051490c2b5aa3abf2ea62eb7fda988
asim sheikh <amazingAsim3000@gmail.com>
2022-04-08 01:17:34 +0530

    hello master

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express=require('express')
 let app=express()
-let port= process.env.PORT || 2000
+let port=( process.env.PORT || 2000)
 let db=require('./config/config')
 let httpserver=require('http').createServer(app)
 let io=require('socket.io')(httpserver)
